7b1661aeef102c9cebbdf3747728d63560c926c4
alessandromarangoni <alessandromarangoni980@gmail.com>
2023-07-08 15:24:22 +0200

    feedback on error (on create)

--- resources/views/comics-form.blade.php
+++ resources/views/comics-form.blade.php
@@ -19,24 +19,35 @@
                 
 
 
-                <label for="name">title</label>
-                <input class="form-control" type="text" name="title">
+                <label for="title">title</label>
+                <input class="form-control @error('title') is-invalid @enderror"  type="text" name="title">
+                @error('title')
+                    <div class="invalid-feedback">{{$message}}</div>
+                @enderror
 
                 <label for="name">description</label>
-                <input class="form-control" type="text" name="description">
-
+                <input class="form-control  @error('description') is-invalid @enderror" type="text" name="description">
+                @error('description')
+                    <div class="invalid-feedback">{{$message}}</div>
+                @enderror
                 <label for="name">thumb</label>
                 <input class="form-control" type="text" name="thumb">
 
                 <label for="name">price</label>
-                <input class="form-control" type="text" name="price">
-
+                <input class="form-control  @error('price') is-invalid @enderror" type="text" name="price">
+                @error('price')
+                    <div class="invalid-feedback">{{$message}}</div>
+                @enderror
                 <label for="name">series</label>
-                <input class="form-control" type="text" name="series">
-
+                <input class="form-control  @error('series') is-invalid @enderror" type="text" name="series">
+                @error('series')
+                    <div class="invalid-feedback">{{$message}}</div>
+                @enderror
                 <label for="name">sale_date</label>
-                <input class="form-control" type="text" name="sale_date">
-
+                <input class="form-control  @error('sale_date') is-invalid @enderror" type="text" name="sale_date">
+                @error('sale_date')
+                    <div class="invalid-feedback">{{$message}}</div>
+                @enderror
                 <input class="form-control mt-4 btn btn-primary" type="submit" value="Crea">
             </form>
         </div>
